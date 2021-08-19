a6fb2b9fd78f4461337c68563542de0207c640e4
Mr Vitalik <50264775+VitalikObject@users.noreply.github.com>
2021-08-19 09:08:13 +0300

    small fixes

---
+++ src/Forza Horizon 4 Helper/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ForzaHorizon4Helper
+{
+    public static class Calculator
+    {
+        public static double Calculate(ComboBox comboBox, double min, double max, int front)
+        {
+            switch(comboBox.Items.IndexOf(comboBox.SelectedItem))
+            {
+                case (int)Side.Front:
+                    return CalculateFrontWeight(min, max, front);
+                case (int)Side.Rear:
+                    return CalculateRearWeight(min, max, front);
+                default:
+                    MessageBox.Show("Selected type doesn't exist!", "Error occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+            return 0;
+        }
+
+        private static double CalculateFrontWeight(double min, double max, int front)
+        {
+            return Math.Round((max - min) * front / 100 + min, 3);
+        }
+
+        private static double CalculateRearWeight(double min, double max, int front)
+        {
+            return Math.Round((max - min) * (100 - front) / 100 + min, 3);
+        }
+
+        private enum Side
+        {
+            Front = 0,
+            Rear = 1
+        }
+    }
+}
---
+++ src/Forza Horizon 4 Helper/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ForzaHorizon4Helper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int front;
+            double min;
+            double max;
+            Int32.TryParse(textBox1.Text, out front);
+            Double.TryParse(textBox3.Text, out min);
+            Double.TryParse(textBox4.Text, out max);
+
+            if (front <= 0 || front > 100)
+            {
+                MessageBox.Show("Entered unusable number!", "Error occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            textBox2.Text = Calculator.Calculate(comboBox1, min, max, front).ToString();
+        }
+    }
+}
